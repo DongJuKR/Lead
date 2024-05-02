b6339379060e77e224c038436df58a125407f319
DongJuKR <102017209+DongJuKR@users.noreply.github.com>
2024-05-02 18:38:47 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function submitForm() {
+    
+    let resultFlag = false;
+
     var inputValue1 = document.getElementById("00NIR000009MUiq").value;
     var inputValue2 = document.getElementById("00NIR000009MQGr").value;
     var inputValue3 = document.getElementById("00NIR000009MQGw").value;
@@ -8,10 +11,11 @@ function submitForm() {
     var inputValue7 = document.getElementById("00NIR000009MQHu").value;
     var inputValue8 = document.getElementById("00NIR000009MQIT").value;
     var inputValue9 = document.getElementById("00NIR000009MQId").value;
-
+    
     if (!inputValue1 || !inputValue2 || !inputValue3 || !inputValue4 || !inputValue5 || 
     !inputValue6 || !inputValue7 || !inputValue8 || !inputValue9) {
       alert("필수 입력 값이 비어 있습니다. 입력 값을 확인해주세요.");
+      return resultFlag;
     } else {
       document.getElementById("myForm").submit();
     }
