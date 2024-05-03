306900fd66943c127597f23c4a3cdcda9f45d123
DongJuKR <102017209+DongJuKR@users.noreply.github.com>
2024-05-03 13:47:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
+// 필수 입력값이 입력됐는지 id 값을 확인하는 스크립트
 function submitForm() {
     
-    let resultFlag = false;
+  let resultFlag = false;
 
-    var inputValue1 = document.getElementById("00NIR000009MUiq").value;
-    var inputValue2 = document.getElementById("00NIR000009MQGr").value;
-    var inputValue3 = document.getElementById("00NIR000009MQGw").value;
-    var inputValue4 = document.getElementById("00NIR000009MV50").value;
-    var inputValue5 = document.getElementById("00NIR000009MQHz").value;
-    var inputValue6 = document.getElementById("email").value;
-    var inputValue7 = document.getElementById("00NIR000009MQHu").value;
-    var inputValue8 = document.getElementById("00NIR000009MQIT").value;
-//    var inputValue9 = document.getElementById("00NIR000009MQId").value;
-    
-    if (!inputValue1 || !inputValue2 || !inputValue3 || !inputValue4 || !inputValue5 || 
-    !inputValue6 || !inputValue7 || !inputValue8 || !inputValue9) {
-      alert("필수 입력 값이 비어 있습니다. 입력 값을 확인해주세요.");
-      return resultFlag;
-    } else {
-      document.getElementById("myForm").submit();
-    }
+  var inputValue1 = document.getElementById("00NIR000009MV5P").value;
+  var inputValue2 = document.getElementById("00NIR000009MV5U").value;
+  var inputValue3 = document.getElementById("00NIR000009MV5Z").value;
+  var inputValue4 = document.getElementById("00NIR000009MV5e").value;
+  var inputValue5 = document.getElementById("00NIR000009MV5j").value;
+  var inputValue7 = document.getElementById("subject").value;
+  
+  if (!inputValue1 || !inputValue2 || !inputValue3 || !inputValue4 || !inputValue5 || 
+  !inputValue6 || !inputValue7) {
+    alert("필수 입력 값이 비어 있습니다. 입력 값을 확인해주세요.");
+    return resultFlag;
+  } else {
+    document.getElementById("myForm").submit();
   }
+}
